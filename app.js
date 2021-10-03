179abb126f914cee5363c2c4a377c3f177e329b1
Arjun-Parzival101 <64570771+Arjun-Parzival101@users.noreply.github.com>
2021-10-03 11:08:02 +0530

    JS updated

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function addTodo(todo) {
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
 
-    li.innerHTML = `<span class="todo-item"> ${todo} </span><button name="checkButton" class="check"><i class="fas fa-check-square"></i></button><button name = "deleteButton" class=""><i class = "fas fa-trash"></i></button>`;
+    li.innerHTML = `<span class="todo-item"> ${todo} </span><button name="checkButton" class="check"><i class="fas fa-check-square"></i></button><button name = "deleteButton" class="delete"><i class = "fas fa-trash"></i></button>`;
 
     li.classList.add('todo-list-item');
     ul.appendChild(li);
